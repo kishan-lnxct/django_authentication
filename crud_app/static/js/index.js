8cff39af7b4547a2669a093b9a32a8dcf2c98ca4
kishan-lnxct <kishan@lnxct.com>
2022-08-22 18:30:37 +0530

    add validation in salary input field that only write numeric value

--- crud_app/static/js/index.js
+++ crud_app/static/js/index.js
@@ -1,16 +1,8 @@
-let emp_id = document.getElementById('emp-id').value;
-console.log("****************88", emp_id)
-alert("ready..")
-// if(typeof emp_id !== 'undefined' && emp_id !== null) {
-//     emp_id.innerHTML = value;
-//   }
-// console.log(emp_id)
-// if (isNaN(emp_id)){
-//     alert("empolyee id is not a number")
-// }
-// function updateHTML(elmId, value) {
-//     var elem = document.getElementById(elmId);
-//     if(typeof elem !== 'undefined' && elem !== null) {
-//       elem.innerHTML = value;
-//     }
-//   }
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+  }
+
+
--- crud_project/settings.py
+++ crud_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "crud_app/static"
+    os.path.join(BASE_DIR, "/crud_app/static"),
 ]
 
 # Default primary key field type
